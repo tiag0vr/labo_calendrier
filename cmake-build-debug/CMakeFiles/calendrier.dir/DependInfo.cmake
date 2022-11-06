78bf4a94d8b3b340210579da90c775d0d1a71a3e
poLionel <115302918+poLionel@users.noreply.github.com>
2022-11-06 18:56:56 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/calendrier.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/DabsHeig/Desktop/labo_calendrier/calendrier.cpp" "CMakeFiles/calendrier.dir/calendrier.cpp.obj" "gcc" "CMakeFiles/calendrier.dir/calendrier.cpp.obj.d"
+  "C:/Users/DabsHeig/Desktop/labo_calendrier/main.cpp" "CMakeFiles/calendrier.dir/main.cpp.obj" "gcc" "CMakeFiles/calendrier.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/calendrier.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calendrier.dir/calendrier.cpp.obj"
+  "CMakeFiles/calendrier.dir/calendrier.cpp.obj.d"
+  "CMakeFiles/calendrier.dir/main.cpp.obj"
+  "CMakeFiles/calendrier.dir/main.cpp.obj.d"
+  "calendrier.exe"
+  "calendrier.exe.manifest"
+  "calendrier.pdb"
+  "libcalendrier.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calendrier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
